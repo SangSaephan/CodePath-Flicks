4bb1ff8e7bda2ff86f859388731c3dd4769fd0e4
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-13 00:10:51 -0800

    User sees loading state when waiting for API

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import EZLoadingActivity
 
 class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -22,23 +23,34 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.dataSource = self
         // Do any additional setup after loading the view.
         
+        EZLoadingActivity.Settings.BackgroundColor = UIColor.black
+        EZLoadingActivity.Settings.TextColor = UIColor.white
+        EZLoadingActivity.Settings.ActivityColor = UIColor.white
+        
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
+                EZLoadingActivity.hide(true, animated: true)
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     print(dataDictionary)
                     self.movies = dataDictionary["results"] as! [NSDictionary]
                     self.tableView.reloadData()
                 }
+            } else {
+                EZLoadingActivity.hide(false, animated: true)
             }
         }
         task.resume()
 
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        EZLoadingActivity.show("Loading...", disableUI: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
